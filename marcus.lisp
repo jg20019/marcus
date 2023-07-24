011fc9bcc98c3ebc54f5544d71951b54eaa6114b
John Gibson <jg20019@gmail.com>
2023-07-24 12:08:20 -0400

    feat(wip): Parse file into blocks.
    According to the parsing strategy found at spec.commonmark.com the frist
    step to parsing markdown is to break the input into blocks. Since the
    only thing I care about is implementing headings and paragraphs to start
    with before potentially implementing the rest of the spect, starting
    with recognizing ATX headings is a good start.

--- marcus.lisp
+++ marcus.lisp
@@ -1,3 +1,28 @@
 ;;;; marcus.lisp
 
 (in-package #:marcus)
+
+(named-readtables:in-readtable :interpol-syntax)
+;;;; https://spec.commonmark.org/0.30/#appendix-a-parsing-strategy
+
+;;;; Parsing has two phases:
+;;;; 1. In the first phase, lines of input are consumed and the block structure
+;;;;    of the document - its division into paragraphs, block quotes, list items
+;;;;    and so on is constructed. Text is assigned to these blocks but not parsed.
+;;;;    Link reference definitions are parsed and a map of links is constructed.
+
+(defun convert-to-blocks (path) 
+  (with-open-file (input path :direction :input) 
+    (loop for line = (read-line input nil nil)
+          while line
+          collect (scan-line line))))
+
+(convert-to-blocks #p"example.md")
+
+(defun scan-line (line) 
+  (cond ((match-heading line) (list 'heading line))
+        (t 'error)))
+
+(defun match-heading (line) 
+  "Check if line matches ATX heading"
+  (cl-ppcre:scan "^#{1,6}\\s+(\.+)$" line))
